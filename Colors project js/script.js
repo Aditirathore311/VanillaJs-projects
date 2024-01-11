82018c86b7a26028793a51fd5c75d6547c5cf893
Aditi rathore <138341983+Aditirathore311@users.noreply.github.com>
2024-01-11 20:46:44 +0530

    Update script.js

--- Colors project js/script.js
+++ Colors project js/script.js
@@ -3,6 +3,7 @@ var index = 0;
 
 document.querySelector("#submit").addEventListener("click", () => {
   if(index > colors.length - 1)
+    
     index = 0;
   document.body.style.backgroundColor = colors[index++];  
 });
